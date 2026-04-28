96a060595c6c1204accb5a1972409e720d2fc99b
hartoksanjaya <hartoksanjaya884@gmail.com>
2026-04-28 23:23:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-const CACHE_NAME = "quran-ai-v1";
+const CACHE_NAME = "quran-ai-v2";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r || fetch(e.request))
   );
 });
